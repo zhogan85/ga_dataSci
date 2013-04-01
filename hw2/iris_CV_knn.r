2fc4a859530065c9cabf4f124e468fc0a505db6f
Zachary Hogan <zhogan85@gmail.com>
2013-04-01 18:56:19 -0400

    Removed some test code from hw2

--- hw2/iris_CV_knn.r
+++ hw2/iris_CV_knn.r
@@ -9,9 +9,9 @@ library(ggplot2)
 data <- iris                # create copy of iris dataframe
 
 N <- nrow(data)          # total number of records (150)
-K <- 10                  # Set n
-K.size <- N/K            # Fold size
-K.count <- 1
+n <- 6                  # Set n
+n.size <- N/n            # Fold size
+n.count <- 1
 
 set.seed(1)        # initialize random seed for consistency
                     # NOTE -- run for various seeds --> need for CV!
@@ -28,9 +28,9 @@ data$Species <- NULL        # remove labels from feature set (note: could
 # APPLY MODEL
 #################################################
 err.rates <- data.frame()  
-for (a in 1:K) {
-     test.start <- (((a-1) * K.size) + 1)    # Start parameter for indexing folds
-     test.end <- (a*K.size)                  # End parameter for indexing folds
+for (a in 1:n) {
+     test.start <- (((a-1) * n.size) + 1)    # Start parameter for indexing folds
+     test.end <- (a*n.size)                  # End parameter for indexing folds
 
      test.index <- test.start:test.end       # One of n folds
 
@@ -40,9 +40,7 @@ for (a in 1:K) {
      train.labels <- as.factor(as.matrix(labels)[-test.index, ])      # extract training set labels
      test.labels <- as.factor(as.matrix(labels)[test.index, ])        # extract test set labels
      
-     #err.rates <- data.frame()       # initialize results object
-
-     max.k <- 20
+     max.k <- 10
      for (k in 1:max.k)              # perform fit for various values of k
      {
          knn.fit <- knn(train = train.data,          # training set
@@ -54,10 +52,10 @@ for (a in 1:K) {
          
          print(table(test.labels, knn.fit))          # print confusion matrix
 
-         this.err <- c(K.count, k, sum(test.labels != knn.fit) / length(test.labels))    # store gzn err
+         this.err <- c(n.count, k, sum(test.labels != knn.fit) / length(test.labels))    # store gzn err
          err.rates <- rbind(err.rates, this.err)     # append err to total results
      }
-     K.count <- K.count + 1
+     n.count <- n.count + 1
 }
 
 
@@ -66,35 +64,22 @@ for (a in 1:K) {
 # OUTPUT RESULTS
 #################################################
 
-#results <- data.frame(1:max.k, err.rates)   # create results summary data frame
-names(err.rates) <- c('K', 'k', 'err.rate')        # label columns of err df
+names(err.rates) <- c('n', 'k', 'err.rate')        # label columns of err df
 
-head(err.rates)
-tail(err.rates)
+err.rates$nk <- paste(err.rates$n, err.rates$k, sep="-")
+results <- data.frame(1:nrow(err.rates), err.rates$err.rate)     # create results summary data frame
+names(results) <- c('nk.count', 'err.rate') 
 
-err.rates[11:20, ]
-
-
-head(err.rates)
-tail(err.rates)
-
-
-#err.rates$Kk <- paste(err.rates$K, err.rates$k, sep="-")
-#results <- data.frame(1:nrow(err.rates), err.rates$err.rate)     # create results summary data frame
-#names(results) <- c('Kk.count', 'err.rate') 
-
-#head(results)
-#tail(results)
 
 #################################################
 # VISUALIZATION - ggplot2
 #################################################
 
 # create title for results plot
-title <- 'K Fold knn results'
+title <- 'n Fold knn results'
 
 # create results plot
-results.plot <- ggplot(err.rates, aes(x=k, y=err.rate)) + geom_point() + geom_line() + facet_wrap(~K, nrow=2, ncol=5)
+results.plot <- ggplot(err.rates, aes(x=k, y=err.rate)) + geom_point() + geom_line() + facet_wrap(~n, nrow=3  )
 results.plot <- results.plot + ggtitle(title)
 
 # draw results plot (note need for print stmt inside script to draw ggplot)
